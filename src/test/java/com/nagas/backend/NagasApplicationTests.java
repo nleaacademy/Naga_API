491c59d1d837d85c11d52b6c6647b16b7dcef213
jestin <nleaacademy@gmail.com>
2023-04-30 20:31:39 +0530

    remove test

--- src/test/java/com/nagas/backend/NagasApplicationTests.java
+++ src/test/java/com/nagas/backend/NagasApplicationTests.java
@@ -1,12 +1,12 @@
 package com.nagas.backend;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class NagasApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
